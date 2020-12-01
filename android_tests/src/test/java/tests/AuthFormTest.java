ebda505730f422f7d87cbbb3c55f1b263ef1fe61
CharlesChaplin666 <74540698+CharlesChaplin666@users.noreply.github.com>
2020-12-01 19:43:09 +0500

    Тест Авторизации
    Позитивный тест авторизации (по логину, емаил и телефону)

--- android_tests/src/test/java/tests/AuthFormTest.java
+++ android_tests/src/test/java/tests/AuthFormTest.java
@@ -19,14 +19,13 @@ public class AuthFormTest extends BaseClass{
         driver.findElementById("biz.growapp.winline:id/etLogin").click();
         driver.findElementById("biz.growapp.winline:id/etLogin").setValue("9745314");
         Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/etLogin").getText(), "9745314");
-
         driver.findElementById("biz.growapp.winline:id/etPassword").click();
         driver.findElementById("biz.growapp.winline:id/etPassword").setValue("123456");
         Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/etPassword").getText(), "123456");
-
         driver.findElementById("biz.growapp.winline:id/btnLogIn").click();
         //Появляется поп-ап Фрибет
         closePopupIfDisplayed();
+        //Выход
         Assertions.assertEquals(driver.findElementsByClassName("android.widget.TextView").get(0).getText(), "Идентификация");
         driver.findElementsById("biz.growapp.winline:id/fixed_bottom_navigation_icon").get(4).click();
         driver.findElementsById("biz.growapp.winline:id/tvText").get(10).click();
@@ -36,7 +35,41 @@ public class AuthFormTest extends BaseClass{
 
         //Переход в раздел Вход через E-MAIL, валидация полей ЛОГИН и ПАРОЛЬ, авторизация и выход из акка
 
+        driver.findElementById("biz.growapp.winline:id/btnAuth").click();
+        Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/etLogin").getText(),"9745314");
+        driver.findElementById("biz.growapp.winline:id/tvAuthType2").click();
+        Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/etLogin").getText(),"E-mail");
+        driver.findElementById("biz.growapp.winline:id/etLogin").setValue("testql1@yandex.ru");
+        Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/etLogin").getText(), "testql1@yandex.ru");
+        driver.findElementById("biz.growapp.winline:id/etPassword").setValue("1234567");
+        Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/etPassword").getText(), "1234567");
+        driver.findElementById("biz.growapp.winline:id/btnLogIn").click();
+        //Выход
+        Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/tvTitle").getText(), "Онлайн-идентификация");
+        driver.findElementsById("biz.growapp.winline:id/fixed_bottom_navigation_icon").get(4).click();
+        driver.findElementsById("biz.growapp.winline:id/tvText").get(10).click();
+        Assertions.assertEquals(driver.findElementById("android:id/message").getText(), "Ты точно хочешь выйти?");
+        driver.findElementById("android:id/button1").click();
+        Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/btnAuth").getText(),"Вход");
+
         //Переход в раздел Вход через ТЕЛЕФОН, валидация полей ЛОГИН и ПАРОЛЬ, авторизация и выход из акка
-        //79172271187
+        driver.findElementById("biz.growapp.winline:id/btnAuth").click();
+        driver.findElementById("biz.growapp.winline:id/tvAuthType1").click();
+        driver.findElementById("biz.growapp.winline:id/etPhoneMask").click();
+        driver.findElementById("biz.growapp.winline:id/etLoginByPhone").setValue("9172271187");
+        Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/etLoginByPhone").getText(), "9172271187");
+        driver.findElementById("biz.growapp.winline:id/etPassword").click();
+        driver.findElementById("biz.growapp.winline:id/etPassword").setValue("123456");
+        Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/etPassword").getText(), "123456");
+        driver.findElementById("biz.growapp.winline:id/btnLogIn").click();
+        //Появляется поп-ап Фрибет
+        closePopupIfDisplayed();
+        //Выход
+        Assertions.assertEquals(driver.findElementsByClassName("android.widget.TextView").get(0).getText(), "Идентификация");
+        driver.findElementsById("biz.growapp.winline:id/fixed_bottom_navigation_icon").get(4).click();
+        driver.findElementsById("biz.growapp.winline:id/tvText").get(10).click();
+        Assertions.assertEquals(driver.findElementById("android:id/message").getText(), "Ты точно хочешь выйти?");
+        driver.findElementById("android:id/button1").click();
+        Assertions.assertEquals(driver.findElementById("biz.growapp.winline:id/btnAuth").getText(),"Вход");
     }
 }
